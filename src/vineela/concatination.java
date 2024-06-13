53582309b43cb9858240b3c573140e846c733794
DELL <DELL@DESKTOP-P1V3UBO>
2024-06-13 22:21:59 +0530

    first comit

---
+++ src/vineela/concatination.java
@@ -0,0 +1,12 @@
+package vineela;
+
+public class concatination {
+	public static void main(String [] args) {
+		int a = 10;
+		int b =60;
+		System.out.println("the values of a and b"+a+b);
+		System.out.println("the values of a and b"+(a+b));
+		System.out.println(a+b+"the value of a and b");
+	}
+
+}
